4b19998ca705641d2a0017617746fcab3733f4ab
MaxChung525 <maxchung525@gmail.com>
2024-09-27 16:16:19 +0800

    Update typed.js

--- typed.js
+++ typed.js
@@ -33,22 +33,16 @@ document.addEventListener("DOMContentLoaded", function () {
     // }
 
     function copyText() {
-        // Get the div element
-        var divElement = document.getElementById("copytextid");
-      
-        // Create a range object
-        var range = document.createRange();
-      
-        // Select the contents of the div element
-        range.selectNode(divElement);
-      
-        // Add the range to the user's selection
-        window.getSelection().addRange(range);
-      
-        // Copy the selected text to the clipboard
-        document.execCommand("copy");
-      
-        // Give a visual feedback to the user that the text has been copied
+        var copyText = "123";
+        
+        // Copy the predetermined text to the clipboard
+        navigator.clipboard.writeText(copyText).then(() => {
+        // Alert the copied text
+            alert("Copied the text: " + copyText);
+        }, (err) => {
+            console.error('Could not copy text: ', err);
+  });
+        
         alert("Text has been copied!");
       }
     
